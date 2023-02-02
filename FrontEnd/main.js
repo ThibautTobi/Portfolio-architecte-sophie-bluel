c8089e9fd7f873cc11597b49d4ee511e94b17b01
thibaut denis <thibaut_du_denis@hotmail.fr>
2023-02-02 19:20:46 +0100

    avancer sur le changement modale

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -29,15 +29,14 @@ for(let i in projets){
 // verifier si il y a un localstorage et si oui faire apparaitre
 
  if (localStorage.getItem("user") !== null){
-  //const local = localStorage.getItem("user")
-  //const localparse = JSON.parse(local)
+  const local = localStorage.getItem("user");
+  const localParse = JSON.parse(local);
+  const token = localParse.token;
+  
   //console.log(local)
-  //console.log(localparse)
-  //if()
-
-
-// ajouter si click sur logout window.localStorage.removeItem("user"); ?
-
+  //console.log(localParse)
+  //console.log(token)
+ 
 
 // je cache les filtres
 const cacherBtn = document.querySelector(".btn-filtre");
@@ -101,7 +100,315 @@ modaleContainer.className = "modale-container modale-container-stop"
 const divModal = document.createElement("div");
 modaleContainer.appendChild(divModal);
 divModal.className= "modale";
+divModal.innerHTML =
+`
+		<button class="btn-close">X</button>
+	<div class="titre-modale">
+		<h3>Galerie photo</h3>
+	</div>
+	<div class="modale-galery"></div>
+	<div class="ligne-modale"></div>
+		<button class="btn-modale">Ajouter une photo</button>
+		<a class="supp-gallery">Supprimer la galerie</a>
+`
 
+// creation des projets en mini pour afficher et modifier
+
+for(let i in projets){
+  let url = projets[i].imageUrl;
+  let title = projets[i].title;
+  let id = projets[i].id;              
+  document.querySelector(".modale-galery").innerHTML +=
+      `
+      <div class="mini-gallery">
+          <div id="${id}">
+              <div class="move-img">
+                <i class="fa-solid fa-arrows-up-down-left-right fleche"></i>
+                <a class="poubelle" id="${id}">
+                  <img class="img-poubelle" src="./assets/icons/poubelle-Vector.svg">
+                </a>
+                <img class="img-mini"crossorigin="anonymous" src=${url} alt="${title}}" >
+              </div>
+              <a class="edit-modale">éditer</a>
+          </div>
+      </div>
+      `
+    };
+
+// jouvre la modal en cliquant sur les boutons modifier 
+
+modif.forEach(btn => btn.addEventListener('click', function (e){
+  e.preventDefault();
+  modaleContainer.style.display = "block";
+}));
+
+// je ferme la modale avec la croix
+const close = document.querySelector(".btn-close");
+close.addEventListener("click",function (e){
+  e.preventDefault();
+  modaleContainer.style.display = "none";
+  modal.classList.toggle("active");
+});
+
+// je ferme la modale en cliquant hors de la modale
+const fermer = function (e){
+  e.preventDefault();
+  divModal.addEventListener("click",function (e){e.stopPropagation();})
+  modaleContainer.style.display = "none";
+  //modal.classList.toggle("active");
+}
+
+modaleContainer.addEventListener("click",fermer);
+
+// modif suppression des projets sur la petites poubelle
+
+const suppProjet = document.querySelectorAll(".poubelle");
+
+const lesMiniGallery = document.querySelectorAll(".mini-gallery");
+/*
+// je fait aparaitre la fleche si c'est le premier élément de la liste  //// rendre dynamique
+const divFleche = document.getElementById("1");
+const fleche = divFleche.querySelector(".move-img .fleche");
+//console.log(divFleche)
+//console.log(fleche)
+fleche.style.display = "block";
+*/
+
+suppProjet.forEach(a => a.addEventListener("click",function(e){
+  e.preventDefault;
+let id = a.getAttribute("id");
+
+
+let divElementId = document.getElementById(`${id}`);
+
+divElementId.parentNode.remove();
+
+  fetch (`http://localhost:5678/api/works/${id}`, {
+      method : 'DELETE',
+      headers :{
+        'content-type' : 'application/json',
+        'Authorization' : `Bearer ${token}`,
+      }}
+      /*
+      .then(reponse => {
+         if (reponse.ok)
+         {alert("la suppression a réussi")
+        }
+      else {
+        alert("Erreur le projet n'est pas supprimer")
+      }})   */ 
+)
+}));
+
+
+// suppression de toutes la gallerie avec le bouton supprimer la galerie
+
+const suppAll = document.querySelector(".supp-gallery");
+
+suppAll.addEventListener("click",function(e){
+  e.preventDefault;
+  //for(let i in lesMiniGallery){
+   // let element = lesMiniGallery[i].firstElementChild;
+    //let elementTarget = element.getparentElement;
+    //console.log(elementTarget)
+    //let id = element.id;
+        //element.remove();
+        //console.log(element)
+        //console.log(id)
+  
+  fetch ("http://localhost:5678/api/works/${id}", {
+      method : 'DELETE',
+      headers :{
+        'content-type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      }});
+      //.then(reponse => {(reponse.ok() => alert("la suppression a réussi")});    
+
+//}
+});
+
+
+
+/*
+suppAll.addEventListener("click",function(e){
+  e.preventDefault;
+  //console.log(lesMiniGallery)
+
+  for(let i in lesMiniGallery){
+    let number = lesMiniGallery[i].length;
+    number.forEach.remove();
+  }
+  
+  //tant qu'il y a encore des projets supprimer 
+  //for( i = 0 ; i < lesMiniGallery.length ; i--){
+  //  lesMiniGallery[i].remove();
+  //}
+
+    /*
+  fetch ("http://localhost:5678/api/works/${id}", {
+      method : 'DELETE',
+      headers :{
+        'content-type' : 'application/json'
+        'Authorization' : localeStorage.user
+      }}
+      //.then(reponse => {(reponse.ok() => alert("la suppression a réussi")});
+      
+);
+  */
+
+
+
+/*
+for(let i in projets){
+  let supp = projets[i].id
+    //for(s = 0 ; s < supp.length ; i--))
+      fetch ("http://localhost:5678/api/works/${id}", {
+          method : 'DELETE',
+          headers :{
+            'content-type' : 'application/json'
+            'Authorization' : `Bearer ${sessionStorage["token"]}`
+          }}
+      .then(reponse => (reponse.ok());
+    }
+
+);
+*/
+
+//});
+
+
+
+// click bouton envoie vers ajout de nouveaux projet modal 2
+
+
+
+
+// modale ajouter nouveaux projets
+// création de la modale 
+const btnAjout = document.querySelector(".btn-modale");
+btnAjout.addEventListener("click", function (e){
+  e.preventDefault;
+/*
+***************************************************************************** faire que la modale ne se ferme pas
+  divModal.addEventListener("click",function (e){e.stopPropagation();})
+  modaleContainer.style.display = "none";
+*/
+
+//divModal.style.display = "none"
+/*
+const divModalAjout = document.createElement("div");
+modaleContainer.appendChild(divModalAjout);
+divModalAjout.className= "modale-ajout";
+ */
+
+//divModal.innerHTML = "";
+divModal.innerHTML =
+  `
+      <div class="ajout-nav">
+        <a class="nav-fleche">
+          <i class="fa-light fa-arrow-left"></i>
+        </a>
+        <a class="nav-croix">X</a>
+      </div>
+      <h3 class="ajout-titre">Ajout photo</h3>
+      <form class="ajout-form">
+        <div class="ajout-photo">
+          <div class="ajout-img">
+            <i class="fa-thin fa-image"></i>
+          </div>
+          <button class="ajout-photo-btn">+ Ajouter photo</button>
+          <p class="ajout-descrip">jpg, png : 4mo max</p>
+        </div>
+        <label class="ajout-label-titre">Titre</label>
+        <input class="ajout-input-titre">
+        <label class="ajout-label-category">Catégorie</label>
+        <select class="ajout-input-category">
+          <option value=""></option>
+          <option value="Objets"></option>
+          <option value="Appartements"></option>
+          <option value="Hôtels & restaurants"></option>
+        </select>
+      </form>
+    <div class="ajout-ligne"></div>
+    <button class="ajout-btn">Valider</button>
+  `
+  });
+
+//<input class="ajout-input-category" ><i class="fa-light fa-angle-down"></i>
+
+
+// evenement click  nouveaux projets
+
+const btnValid = document.getElementsByClassName("ajout-btn");
+console.log(btnValid)
+//const validProjet =
+/*
+ .addEventListener("click", function (e){
+  e.preventDefault;
+ });*/
+/*
+// publication de nouveau projets  ********************************************* créé une img ou div pour remplacer et faire apparaitre l'image a upload
+
+const formData = new FormData();
+const fileField = document.querySelector('input[type="file"]');
+
+formData.append('image' = null);
+formData.append('titre'= null);
+formData.append('category'= null);
+
+fetch('http://localhost:5678/api/works', {
+  method: 'POST',
+  headers: {
+            'accept' : 'application/json',
+            'content-type' : 'multipart/form-data',
+            } 
+  body: formData
+})
+  .then((response) => response.json())
+  .then((result) => {
+    console.log('Success:', result);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+*/
+ };
+
+ /*
+innerHTML = 
+`
+	<div class="ajout-modale"></div>
+		<div class="ajout-nav">
+			<a class="nav-fleche"></a>
+			<a class="nav-croix"></a>
+		</div>
+		<h3 class="ajout-titre">Ajout photo</h3>
+		<form class="ajout-form">
+			<div class="ajout-photo">
+        <img class="ajout-img">
+				<button>+ Ajouter photo</button>
+        <p class="ajout-descrip">jpg, png : 4mo max</p>
+		</div>
+    <label class="ajout-label-titre">
+		<input class="ajout-input-titre">
+    <label class="ajout-label-category">
+		<input class="ajout-input-category">
+		</form>
+	<div class="ajout-ligne">
+	</div>
+	<button class="ajput-btn">Valider</button>
+	</div>
+`
+*/
+
+
+
+
+
+
+
+/**************************************** premiere creation modale sans inner html ****************************************************/
+/*
 const closeModale = document.createElement("button");
 divModal.appendChild(closeModale);
 closeModale.className = "btn-close";
@@ -152,247 +459,4 @@ const suppModale = document.createElement("a");
 divModal.appendChild(suppModale);
 suppModale.className = "supp-gallery";
 suppModale.innerHTML = "Supprimer la galerie";
-
-// jouvre la modal en cliquant sur les boutons modifier 
-
-modif.forEach(btn => btn.addEventListener('click', function (e){
-  e.preventDefault();
-  modaleContainer.style.display = "block";
-  modal.classList.toggle("active");
-}));
-
-// je ferme la modale avec la croix
-closeModale.addEventListener("click",function (e){
-  e.preventDefault();
-  modaleContainer.style.display = "none";
-  modal.classList.toggle("active");
-});
-
-// je ferme la modale en cliquant hors de la modale
-const fermer = function (e){
-  e.preventDefault();
-  divModal.addEventListener("click",function (e){e.stopPropagation();})
-  modaleContainer.style.display = "none";
-  modal.classList.toggle("active");
-}
-
-modaleContainer.addEventListener("click",fermer);
-
-// modif suppression des projets sur la petites poubelle
-
-const suppProjet = document.querySelectorAll(".poubelle");
-
-const lesMiniGallery = document.querySelectorAll(".mini-gallery");
-
-// je fait disparaitre les fleche si ce n'est pas le premier élément
-   
-//console.log()
-
-suppProjet.forEach(a => a.addEventListener("click",function(e){
-  e.preventDefault;
-let id = a.getAttribute("id");
-//console.log(id)
-
-
-/*
-
-
- voir .id probleme console 
-
-
-
 */
-/*
-for(let i in lesMiniGallery){
-  
-   let element = lesMiniGallery[i].firstElementChild;
-  
-   //console.log(element)
-  
-     if (id === element.id){
-        element.parentElement.remove();
-        }
-}
-*/
-/*
-  fetch ("http://localhost:5678/api/works/${id}", {
-      method : 'DELETE',
-      headers :{
-        'content-type' : 'application/json'
-        'Authorization' : 'user'
-      }}
-      //.then(reponse => {(reponse.ok() => alert("la suppression a réussi")});
-      
-);
-*/
-}));
-
-// suppression de toutes la gallerie avec le bouton supprimer la galerie
-
-const suppAll = document.querySelector(".supp-gallery");
-
-suppAll.addEventListener("click",function(e){
-  e.preventDefault;
-  for(let i in lesMiniGallery){
-    let element = lesMiniGallery[i].firstElementChild;
-    //let id = element.id;
-        element.remove();
-        //console.log(element)
-        //console.log(id)
-  /*
-  fetch ("http://localhost:5678/api/works/${id}", {
-      method : 'DELETE',
-      headers :{
-        'content-type' : 'application/json'
-        'Authorization' : 'user'
-      }}
-      //.then(reponse => {(reponse.ok() => alert("la suppression a réussi")});
-      
-);
-*/
-/*
-for(let i = 0 ; i < lesMiniGallery.length ; i--){
-*/
-}
-
-});
-
-
-
-/*
-suppAll.addEventListener("click",function(e){
-  e.preventDefault;
-  //console.log(lesMiniGallery)
-
-  for(let i in lesMiniGallery){
-    let number = lesMiniGallery[i].length;
-    number.forEach.remove();
-  }
-  
-  //for( i = 0 ; i < lesMiniGallery.length ; i--){
-  //  lesMiniGallery[i].remove();
-  //}
-*/
-
-
-/*
-for(let i in projets){
-  let supp = projets[i].id
-    //for(s = 0 ; s < supp.length ; i--))
-      fetch ("http://localhost:5678/api/works/${id}", {
-          method : 'DELETE',
-          headers :{
-            'content-type' : 'application/json'
-            'Authorization' : `Bearer ${sessionStorage["token"]}`
-          }}
-      .then(reponse => (reponse.ok());
-    }
-
-);
-*/
-
-//});
-
-
-
-// click bouton envoie vers ajout de nouveaux projet modal 2
-
-/*
-btn.addEventListener("click",function (e){
-  .modale-ajout
-})
-*/
-
-
-// modale ajouter nouveaux projets
-// création de la modale 
-
-btnModale.addEventListener("click", function (e){
-  e.preventDefault;
-  /*
-  divModal.innerHTML = 
-  `
-    <div class="ajout-modale"></div>
-      <div class="ajout-nav">
-        <a class="nav-fleche"></a>
-        <a class="nav-croix"></a>
-      </div>
-      <h3 class="ajout-titre">Ajout photo</h3>
-      <form class="ajout-form">
-        <div class="ajout-photo">
-          <img class="ajout-img">
-          <button>+ Ajouter photo</button>
-          <p class="ajout-descrip">jpg, png : 4mo max</p>
-      </div>
-      <label class="ajout-label-titre">
-      <input class="ajout-input-titre">
-      <label class="ajout-label-category">
-      <input class="ajout-input-category">
-      </form>
-    <div class="ajout-ligne">
-    </div>
-    <button class="ajput-btn">Valider</button>
-    </div>
-  `*/
-})
-/*
-innerHTML = 
-`
-	<div class="ajout-modale"></div>
-		<div class="ajout-nav">
-			<a class="nav-fleche"></a>
-			<a class="nav-croix"></a>
-		</div>
-		<h3 class="ajout-titre">Ajout photo</h3>
-		<form class="ajout-form">
-			<div class="ajout-photo">
-        <img class="ajout-img">
-				<button>+ Ajouter photo</button>
-        <p class="ajout-descrip">jpg, png : 4mo max</p>
-		</div>
-    <label class="ajout-label-titre">
-		<input class="ajout-input-titre">
-    <label class="ajout-label-category">
-		<input class="ajout-input-category">
-		</form>
-	<div class="ajout-ligne">
-	</div>
-	<button class="ajput-btn">Valider</button>
-	</div>
-`
-*/
-
-
-// evenement click  nouveaux projets
-
-
-
-
-/*
-// publication de nouveau projets
-
-const formData = new FormData();
-const fileField = document.querySelector('input[type="file"]');
-
-formData.append('image' = null);
-formData.append('titre'= null);
-formData.append('category'= null);
-
-fetch('http://localhost:5678/api/works', {
-  method: 'POST',
-  headers: {
-            'accept' : 'application/json',
-            'content-type' : 'multipart/form-data',
-            } 
-  body: formData
-})
-  .then((response) => response.json())
-  .then((result) => {
-    console.log('Success:', result);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-*/
-//}
-};
